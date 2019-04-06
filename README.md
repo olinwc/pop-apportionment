7c6445b592fcb4eae3eb2f024172f60923b8d83d
Olin Calvin <calvinolinw@gmail.com>
2019-04-05 21:54:40 -0400

    Test push

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pop-apportionment
+Started by Olin William Calvin on 5 April 2019
+
 Short program for calculating apportionment, like for the US House of Representatives.
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+  std::cout << "Hello There" << std::endl;
+  return 0;
+}
