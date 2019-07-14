8e7239c0ce997979faf3febab98c0b1187aadbef
Olin Calvin <calvinolinw@gmail.com>
2019-07-14 13:25:45 -0600

    1870-2000 US Census and small function changes

--- main.cpp
+++ main.cpp
@@ -7,7 +7,160 @@
 
 using namespace std;
 
-int main()
+void
+hamiltonMethod(unsigned int & assigned_seats,
+               unsigned int & total_seats,
+               vector<unsigned int> & seat_vector,
+               const vector<unsigned int> & pop_vector,
+               const double & ratio)
+{
+  // Calculated representation
+  double h_ratio = 0.0;
+  // Index of the state with the highest underrepresentation ratio
+  unsigned int h_ratio_index = 0;
+  while (assigned_seats != total_seats)
+  {
+    for (unsigned int i = 0; i != pop_vector.size(); ++i)
+    {
+      double in_ratio = pop_vector[i] / ratio;
+      in_ratio -= static_cast<double>(seat_vector[i]);
+
+      if (in_ratio > h_ratio)
+      {
+        h_ratio = in_ratio;
+        h_ratio_index = i;
+      }
+    }
+    seat_vector[h_ratio_index] = seat_vector[h_ratio_index] + 1;
+    h_ratio = 0.0;
+    assigned_seats++;
+  }
+}
+
+void
+huntingtonHillMethod(unsigned int & assigned_seats,
+                     unsigned int & total_seats,
+                     const unsigned int & population,
+                     const unsigned int & ideal_ratio,
+                     vector<unsigned int> & seat_vector,
+                     const vector<unsigned int> & pop_vector)
+{
+  // Calculated representation
+  double hh_ratio = 0.0;
+  // Index of the state with the highest underrepresentation ratio
+  unsigned int hh_ratio_index = 0;
+  while (assigned_seats < total_seats)
+  {
+    for (unsigned int i = 0; i != pop_vector.size(); ++i)
+    {
+      double in_ratio = pop_vector[i] / pow((seat_vector[i] * (seat_vector[i] + 1)), 0.5);
+
+      if (in_ratio > hh_ratio)
+      {
+        hh_ratio = in_ratio;
+        hh_ratio_index = i;
+      }
+    }
+    seat_vector[hh_ratio_index] = seat_vector[hh_ratio_index] + 1;
+    hh_ratio = 0.0;
+    assigned_seats++;
+  }
+  if (assigned_seats > total_seats)
+    cout << "WARNING: Allocated Huntington-Hill method with " << assigned_seats << " instead of " << total_seats << "\n\n";
+}
+
+int
+jeffersonMethod(unsigned int & assigned_seats,
+                unsigned int & total_seats,
+                const unsigned int & population,
+                vector<unsigned int> & seat_vector,
+                const vector<unsigned int> & pop_vector,
+                const bool & debug,
+                const unsigned int & option,
+                const unsigned int & ideal_ratio)
+{
+  unsigned int j_threshold = 0;
+  if (option == 1)
+  {
+    // Number of times the while loop is performed
+    unsigned int num_jefferson_loops = 0;
+    j_threshold = population / total_seats;
+    while (assigned_seats < total_seats)
+    {
+      num_jefferson_loops++;
+      for (unsigned int i = 0; i != pop_vector.size(); ++i)
+        if (pop_vector[i] > j_threshold)
+          seat_vector[i] = pop_vector[i] / j_threshold;
+        assigned_seats = 0;
+        for (unsigned int i = 0; i != pop_vector.size(); ++i)
+          assigned_seats += seat_vector[i];
+        if (assigned_seats > total_seats)
+          cout << "WARNING: Allocated Jefferson method with " << assigned_seats << " instead of " << total_seats << "!\n\n";
+        else if (assigned_seats < total_seats && j_threshold == 1)
+        {
+          cout << "ERROR: Cannot allocate Jefferson method with given inputs, closing program\n";
+          return 1;
+        }
+        else
+        {
+          if (debug)
+            cout << "Recalculating Jefferson method\n\n";
+          // FIXME: Reduce the threshold by 1% and then recalculate
+          // This may need to be adjusted in the future
+          j_threshold = j_threshold - (j_threshold * 0.01);
+      }
+    }
+    if (debug)
+      cout << "Required " << num_jefferson_loops << " loops\n\n";
+  }
+  else if (option == 2)
+  {
+    j_threshold = ideal_ratio;
+    assigned_seats = 0;
+    for (unsigned int i = 0; i != pop_vector.size(); ++i)
+    {
+      if (pop_vector[i] > ideal_ratio)
+        seat_vector[i] = pop_vector[i] / ideal_ratio;
+      assigned_seats += seat_vector[i];
+    }
+  }
+  return 0;
+}
+
+void
+websterMethod(unsigned int & assigned_seats,
+              unsigned int & total_seats,
+              vector<unsigned int> & seat_vector,
+              const vector<unsigned int> & pop_vector,
+              const unsigned int & option,
+              const unsigned int & ideal_ratio,
+              const unsigned int & ratio)
+{
+  double webster_seats = 0.0;
+  assigned_seats = 0;
+  for (unsigned int i = 0; i != pop_vector.size(); ++i)
+  {
+    if (option == 1)
+      if (pop_vector[i] > ratio)
+      {
+        webster_seats = pop_vector[i] / ratio;
+        seat_vector[i] = round(webster_seats);
+      }
+    else if (option == 2)
+      if (pop_vector[i] > ideal_ratio)
+      {
+        webster_seats = pop_vector[i] / ideal_ratio;
+        seat_vector[i] = round(webster_seats);
+      }
+    assigned_seats += seat_vector[i];
+  }
+  // There is the possibility of not being able to match the specified number of seats with the Webster method
+  if (option == 1 && assigned_seats != total_seats)
+    cout << "WARNING: Allocated Webster method with " << assigned_seats << " instead of " << total_seats << "!\n";
+}
+
+int
+main()
 {
   // Declare variables
   // Whether or not to run in debug mode
@@ -45,6 +198,7 @@ int main()
   cout << "2: Hamilton method\n";
   cout << "3: Jefferson method\n";
   cout << "4: Webster method\n";
+  cout << "9: All methods for Latex output\n";
   cout << '\n';
   cout << "Please enter a number between 1 and 4:\n\n";
   cin >> method;
@@ -57,6 +211,8 @@ int main()
     cout << "Jefferson method selected\n\n";
   else if (method == 4)
     cout << "Webster method selected\n\n";
+  else if (method == 9)
+    cout << "Running all methods\n\n";
   else
   {
     cout << "ERROR: Invalid input of " << method << " entered, closing program\n";
@@ -150,6 +306,7 @@ int main()
     }
     else if (ideal_ratio >= population / pop_vector.size())
       cout << "WARNING: People per seat ratio of " << ideal_ratio << " is greater than the total population divided by the number of states" << "\n\n";
+    total_seats = population / ideal_ratio;
   }
 
   // Total number of seats that have been assigned
@@ -166,133 +323,49 @@ int main()
   // Method 1: The Huntington-Hill method
   if (method == 1)
   {
-    // Calculated representation
-    double hh_ratio = 0.0;
-    // Index of the state with the highest underrepresentation ratio
-    unsigned int hh_ratio_index = 0;
-    // If ratio was specified, calculate total_seats from the ratio
-    if (option == 2)
-      total_seats = population / ideal_ratio;
-    while (assigned_seats < total_seats)
-    {
-      for (unsigned int i = 0; i != pop_vector.size(); ++i)
-  		{
-  			double in_ratio = pop_vector[i] / pow((seat_vector[i] * (seat_vector[i] + 1)), 0.5);
-
-  			if (in_ratio > hh_ratio)
-  			{
-  				hh_ratio = in_ratio;
-  				hh_ratio_index = i;
-  			}
-  		}
-  		seat_vector[hh_ratio_index] = seat_vector[hh_ratio_index] + 1;
-  		hh_ratio = 0.0;
-  		assigned_seats++;
-    }
-    if (assigned_seats > total_seats)
-      cout << "WARNING: Allocated Huntington-Hill method with " << assigned_seats << " instead of " << total_seats << "\n\n";
+    huntingtonHillMethod(assigned_seats,
+                         total_seats,
+                         population,
+                         ideal_ratio,
+                         seat_vector,
+                         pop_vector);
   }
   // Method 2: The Hamilton method
   else if (method == 2)
   {
-    // Calculated representation
-    double h_ratio = 0.0;
-    // Index of the state with the highest underrepresentation ratio
-    unsigned int h_ratio_index = 0;
-    // If ratio was specified, calculate total_seats from the ratio
-    if (option == 2)
-      total_seats = population / ideal_ratio;
-    while (assigned_seats != total_seats)
-    {
-      for (unsigned int i = 0; i != pop_vector.size(); ++i)
-  		{
-  			double in_ratio = pop_vector[i] / ratio;
-  			in_ratio -= static_cast<double>(seat_vector[i]);
-
-  			if (in_ratio > h_ratio)
-  			{
-  				h_ratio = in_ratio;
-  				h_ratio_index = i;
-  			}
-  		}
-  		seat_vector[h_ratio_index] = seat_vector[h_ratio_index] + 1;
-  		h_ratio = 0.0;
-  		assigned_seats++;
-    }
+    hamiltonMethod(assigned_seats,
+                   total_seats,
+                   seat_vector,
+                   pop_vector,
+                   ratio);
   }
   // Method 3: The Jefferson method
   else if (method == 3)
   {
-    unsigned int j_threshold = 0;
-    if (option == 1)
+    unsigned int error_check = jeffersonMethod(assigned_seats,
+                                               total_seats,
+                                               population,
+                                               seat_vector,
+                                               pop_vector,
+                                               debug,
+                                               option,
+                                               ideal_ratio);
+    // Close program if error check is greater than 0s
+    if (error_check == 1)
     {
-      // Number of times the while loop is performed
-      unsigned int num_jefferson_loops = 0;
-      j_threshold = population / total_seats;
-      while (assigned_seats < total_seats)
-      {
-        num_jefferson_loops++;
-        for (unsigned int i = 0; i != pop_vector.size(); ++i)
-          if (pop_vector[i] > j_threshold)
-            seat_vector[i] = pop_vector[i] / j_threshold;
-          assigned_seats = 0;
-          for (unsigned int i = 0; i != pop_vector.size(); ++i)
-            assigned_seats += seat_vector[i];
-          if (assigned_seats > total_seats)
-            cout << "WARNING: Allocated Jefferson method with " << assigned_seats << " instead of " << total_seats << "!\n\n";
-          else if (assigned_seats < total_seats && j_threshold == 1)
-          {
-            cout << "ERROR: Cannot allocate Jefferson method with given inputs, closing program\n";
-            return 0;
-          }
-          else
-          {
-            if (debug)
-              cout << "Recalculating Jefferson method\n\n";
-            // Reduce the threshold by 1% and then recalculate
-            // This may need to be adjusted in the future
-            j_threshold = j_threshold - (j_threshold * 0.01);
-        }
-      }
-      if (debug)
-        cout << "Required " << num_jefferson_loops << " loops\n\n";
-    }
-    else if (option == 2)
-    {
-      j_threshold = ideal_ratio;
-      assigned_seats = 0;
-      for (unsigned int i = 0; i != pop_vector.size(); ++i)
-      {
-        if (pop_vector[i] > ideal_ratio)
-          seat_vector[i] = pop_vector[i] / ideal_ratio;
-        assigned_seats += seat_vector[i];
-      }
+      return 0;
     }
   }
   // Method 4: The Webster method
   else if (method == 4)
   {
-    double webster_seats = 0.0;
-    assigned_seats = 0;
-    for (unsigned int i = 0; i != pop_vector.size(); ++i)
-    {
-      if (option == 1)
-        if (pop_vector[i] > ratio)
-        {
-          webster_seats = pop_vector[i] / ratio;
-          seat_vector[i] = round(webster_seats);
-        }
-      else if (option == 2)
-        if (pop_vector[i] > ideal_ratio)
-        {
-          webster_seats = pop_vector[i] / ideal_ratio;
-          seat_vector[i] = round(webster_seats);
-        }
-      assigned_seats += seat_vector[i];
-    }
-    // There is the possibility of not being able to match the specified number of seats with the Webster method
-    if (option == 1 && assigned_seats != total_seats)
-      cout << "WARNING: Allocated Webster method with " << assigned_seats << " instead of " << total_seats << "!\n";
+    websterMethod(assigned_seats,
+                  total_seats,
+                  seat_vector,
+                  pop_vector,
+                  option,
+                  ideal_ratio,
+                  ratio);
   }
 
   cout << "What is the name of the output file (please end it with the .csv extension)?\n\n";
